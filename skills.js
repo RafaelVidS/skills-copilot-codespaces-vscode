6c8543f5b6f4dd3262645d3f9d7aa8d21ac3eb57
RafaelVidS <rafaelvidentiosuryasubrata@mail.ugm.ac.id>
2025-04-30 01:40:44 +0000

    copilot first commit

---
+++ skills.js
@@ -0,0 +1,45 @@
+function calculateNumbers(var1, var2)
+{
+    return var1 + var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 - var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 * var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 / var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 % var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 ** var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 & var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 | var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 ^ var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 << var2;
+}
+function calculateNumbers(var1, var2)
+{
+    return var1 >> var2;
+}
+function calculateNumbers(var1, var2)
